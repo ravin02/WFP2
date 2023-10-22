fd94a6e727db2e8bf51e4ae888b0635530236878
PrettyJRicky <patrickjayestrada@yahoo.com>
2023-10-22 16:44:19 +0200

    Tailwind CSS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,13 @@
-Hallo
-
-Hallo Ravin
-
-Hey wie gehts
-
-Danke gut und dir ?
-
-Gut mein Schatz
-
-Hey bruder
-
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world! Hallo wie gehts
+  </h1>
+</body>
+</html>
